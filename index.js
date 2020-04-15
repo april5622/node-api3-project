9f5cd6927d85a08def4f3b63b965fabc0c5371c0
April Ma <54913304+april5622@users.noreply.github.com>
2020-04-15 14:52:17 -0700

    MVP done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code away!
+const express = require("express");
+const cors = require("cors");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+const logger = require("./middleware/logger");
+
+const server = express();
+const port = 5000;
+
+server.use(express.json());
+server.use(cors());
+server.use(logger({format: "long"}))
+
+server.use("/api/users", userRouter);
+server.use("/api/users/:id/posts", postRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "ERROR ERROR"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
+
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,127 @@
 const express = require('express');
+const users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser(), (req, res) => {
+  users.insert(req.body)
+    .then((user) =>{
+      res.status(201).json(user)
+    })
+    .catch((error) => {
+      next(error)
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId(),validatePost(), (req, res) => {
+  res.status(200).json(req.user)
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  const options = {
+    sortBy: req.query.sortBy,
+    limit: req.query.limit
+  }
+  users.get(options)
+    .then((users) => {
+      res.status(200).json(users)
+    })
+    .catch((error) =>{
+      console.log(error)
+      next(error)
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId(), (req, res) => {
+  res.status(200).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId(), validatePost(), (req, res) => {
+  res.status(200).json(req.user)
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId(), (req, res) => {
+  users.remove(req.params.id)
+    .then((count) => {
+      res.status(200).json(count)
+    })
+    .catch((error) => {
+      next(error)
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId(), (req, res) => {
+ users.update(req.params.id, req.body)
+  .then((user) => {
+    res.status(200).json(user)
+  })
+  .catch((error) => {
+    next(error)
+  })
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId() {
+  return(req, res, next) => {
+    users.getById(req.params.id)
+    .then((user) => {
+      if(user){
+        req.user= user
+        next()
+      } else {
+        res.status(400).json({
+            message: "invalid user id"
+        })
+      }
+    })
+    .catch((error) => {
+      next(error)
+    })
+  }  
 }
 
-function validateUser(req, res, next) {
-  // do your magic!
+function validateUser() {
+  return (req, res, next)=> {
+    if(!req.body.name){
+      return res.status(400).json({
+        message: "missing required name field"
+      })
+    }
+    users.insert(!req.body)
+      .then((user) => {
+        res.status(201).json(user)
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(400).json({
+          message: "missing user data"
+        })
+      })
+    next();
+
+  }
 }
 
-function validatePost(req, res, next) {
-  // do your magic!
+function validatePost() {
+  return (req, res, next) => {
+    if(!req.body.text){
+      return res.status(400).json({
+        message: "missing required text field"
+      })
+    }
+    users.getUserPosts(req.params.id)
+      .then((post) => {
+        res.status(200).json(post)
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(400).json({
+          message:"missing post data"
+        })
+      })
+    next();
+  } 
 }
 
 module.exports = router;
